80d580eefeaae2de52eaaa202db22719fc82c8d8
hansel.suarez <hansel.suarez@pragma.com.co>
2018-01-12 23:29:04 -0500

    Separar las rutas hijas a un archivo especifico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,22 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //importamos el componente que queremos agregarle el routing
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
+
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     //El elemento llamado children permite manejar rutas hijas
     {
         path: 'usuario/:id', component: UsuarioComponent,
-        children: [
+        children: USUARIO_ROUTES
+        /*children: [
             { path: 'nuevo', component: UsuarioNuevoComponent },
             { path: 'editar', component: UsuarioEditarComponent },
             { path: 'detalle', component: UsuarioDetalleComponent },
             { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-        ]
+        ]*/
     },
 
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
