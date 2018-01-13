fbc15a3b42ad7ec9165742d78622628bffc62b96
hansel.suarez <hansel.suarez@pragma.com.co>
2018-01-12 23:20:59 -0500

    Rutas Hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 //importamos el componente que queremos agregarle el routing
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    //El elemento llamado children permite manejar rutas hijas
+    {
+        path: 'usuario/:id', component: UsuarioComponent,
+        children: [
+            { path: 'nuevo', component: UsuarioNuevoComponent },
+            { path: 'editar', component: UsuarioEditarComponent },
+            { path: 'detalle', component: UsuarioDetalleComponent },
+            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+        ]
+    },
+
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
